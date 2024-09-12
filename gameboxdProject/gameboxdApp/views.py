c194104815be7dec91987e1ee5ea2a22127bdb52
Slinky0812 <ed20a2am@leeds.ac.uk>
2024-09-12 14:27:17 +0100

    Added templates using bootstrap css

--- gameboxdProject/gameboxdApp/views.py
+++ gameboxdProject/gameboxdApp/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Abdullah0812',
+        'title': 'Elden Ring',
+        'content': 'Great game',
+        'date_posted': '27/08/2024'
+    },
+        {
+        'author': 'Abdullah0812',
+        'title': 'Tekken 8',
+        'content': 'Boring game',
+        'date_posted': '26/08/2024'
+    }
+]
 
 # Home view
 def home(request):
-    return HttpResponse('<h1>Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'gameboxdApp/home.html', context=context)
 
 
 # About view
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    context = {'title': 'About'}
+    return render(request, 'gameboxdApp/about.html', context=context)
