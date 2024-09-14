aac3e8abb179d14192aaf4914bf7fe4c2db2c719
Slinky0812 <ed20a2am@leeds.ac.uk>
2024-09-14 13:03:12 +0100

    Created database tables and added reviews

--- gameboxdProject/gameboxdApp/models.py
+++ gameboxdProject/gameboxdApp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# database table for the games a user can review
+class Game(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+# database table for a user's reviews
+class Review(models.Model):
+    title = models.ForeignKey(Game, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    like = models.BooleanField(default=False)
+    review = models.TextField()
+    datePosted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
--- gameboxdProject/gameboxdApp/views.py
+++ gameboxdProject/gameboxdApp/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
+from .models import Review
 
-posts = [
-    {
-        'author': 'Abdullah0812',
-        'title': 'Elden Ring',
-        'content': 'Great game',
-        'date_posted': '27/08/2024'
-    },
-        {
-        'author': 'Abdullah0812',
-        'title': 'Tekken 8',
-        'content': 'Boring game',
-        'date_posted': '26/08/2024'
-    }
-]
 
 # Home view
 def home(request):
     context = {
-        'posts': posts
+        'posts': Review.objects.all()
     }
     return render(request, 'gameboxdApp/home.html', context=context)
 
